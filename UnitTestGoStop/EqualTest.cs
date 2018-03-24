c52341f5da9423085d1dcbb0e88afe7c082029a6
Alex Lee <asylee1005@gmail.com>
2018-03-24 07:10:03 -0500

    add Unit Test

---
+++ UnitTestGoStop/EqualTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GoStop.Card;
+
+namespace UnitTestGoStop
+{
+    [TestClass]
+    public class EqualTest
+    {
+        Hanafuda hanafuda;
+
+        [TestMethod]
+        public void KwangEquals()
+        {
+            //initialization for test
+            hanafuda = new Kwang(Month.April);
+            Hanafuda toCompare = new Kwang(Month.April);
+            //true test
+            Assert.AreEqual(true, hanafuda == toCompare);
+            //false test
+            Assert.AreEqual(false, hanafuda != toCompare);
+        }
+
+        [TestMethod]
+        public void TtiEquals()
+        {
+            hanafuda = new HongDan(Month.April);
+            //same month wrong dan
+            Hanafuda toCompare = new ChungDan(Month.April);
+            Assert.AreEqual(false, hanafuda == toCompare);
+            //same card
+            toCompare = new HongDan(Month.April);
+            Assert.AreEqual(true, hanafuda == toCompare);
+        }
+    }
+}
---
+++ UnitTestGoStop/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestGoStop")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestGoStop")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c34db0cb-47ed-40a8-bbc2-67aa748f43a3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
