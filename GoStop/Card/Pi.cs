1ab183493fc89226ddbcd6d485a551a820dca185
Alex Lee <asylee1005@gmail.com>
2018-03-24 07:11:10 -0500

    add new type of cards

---
+++ GoStop/Card/Pi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoStop.Card
+{
+    public enum PiType { pi=1, SsangPi }
+
+    public class Pi : Hanafuda
+    {
+        PiType point;
+
+        public int Point { get => (int)point; }
+
+        public Pi(Month _month) : this(_month, PiType.pi)
+        { }
+
+        protected Pi(Month _month, PiType _point) : base(_month, CardType.Pi)
+        {
+            point = _point;
+        }
+
+        protected override bool isEqual(Hanafuda other)
+        {
+            if (base.isEqual(other) 
+                && this.Point == ((Pi)other).Point)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ GoStop/Card/SsangPi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoStop.Card
+{
+    class SsangPi : Pi
+    {
+        public SsangPi(Month _month) : base(_month, PiType.SsangPi)
+        { }
+    }
+}
