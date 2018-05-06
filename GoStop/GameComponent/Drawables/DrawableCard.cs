a9018bf7be9cf357ec298c9081c0ecc2c5f3c9fc
Alex Lee <asylee1005@gmail.com>
2018-05-06 00:00:28 -0500

    add initial GameComponents

---
+++ GoStop/GameComponent/Drawables/DrawableCard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+using MG_Library;
+using GoStop.Card;
+
+namespace GoStop.GameComponent.Drawables
+{
+    public class DrawableCard : DrawableGameComponent
+    {
+        private Hanafuda _card;
+        private Sprite2D _image;
+
+        public DrawableCard(Game game, Hanafuda card, Sprite2D image) : base(game)
+        {
+            _card = card;
+            _image = image;
+        }
+
+        public override void Initialize()
+        {
+            _image.Position
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        protected override void OnDrawOrderChanged(object sender, EventArgs args)
+        {
+            base.OnDrawOrderChanged(sender, args);
+        }
+
+        protected override void OnVisibleChanged(object sender, EventArgs args)
+        {
+            base.OnVisibleChanged(sender, args);
+        }
+
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+    }
+}
---
+++ GoStop/GameComponent/HanafudaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+using MG_Library;
+using GoStop.Card;
+
+namespace GoStop
+{
+    internal class MonogameController : GameComponent
+    {
+        private IInputManager inputManager;
+        private Hanafuda selected;
+
+        public MonogameController(Game game) : base(game)
+        {
+            inputManager = Game.Services.GetService<IInputManager>();
+        }
+    }
+}
