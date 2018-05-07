dfbdb1e819aa21fad496f0d8e939925d3fe2c529
Alex Lee <asylee1005@gmail.com>
2018-05-07 03:48:26 -0500

    initial TestSprite commit

---
+++ GoStop/MonoGameComponents/TestSprite.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+using MG_Library;
+
+namespace GoStop.MonoGameComponents
+{
+    public class TestSprite : Sprite2D
+    {
+        public TestSprite(Game game) : base(game, null)
+        { }
+    }
+}
