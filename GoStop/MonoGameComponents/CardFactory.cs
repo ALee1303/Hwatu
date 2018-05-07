afd5191bf0706890dbf34caa7a2d4e776c268334
Alex Lee <asylee1005@gmail.com>
2018-05-07 05:16:32 -0500

    initial CardFactory commit

---
+++ GoStop/MonoGameComponents/CardFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+using MG_Library;
+using GoStop.Card;
+using GoStop.MonoGameComponents.Drawables;
+
+namespace GoStop.MonoGameComponents
+{
+    public class CardFactory : GameComponent
+    {
+        private Dictionary<KeyValuePair<Month, CardType>, Sprite2D> spriteGallery;
+        private BoardManager manager;
+
+        public CardFactory(Game game) : base(game)
+        {
+            manager = Game.Services.GetService<BoardManager>();
+        }
+
+        public void LoadTestImage()
+        {
+        }
+
+        public DrawableCard ReturnedPairedDrawable(Hanafuda card)
+        {
+            KeyValuePair<Month, CardType> idx =
+                new KeyValuePair<Month, CardType>(card.Month, card.Type);
+            Sprite2D image = spriteGallery[idx];
+            return new DrawableCard(Game, card, image);
+        }
+    }
+}
